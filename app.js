a48b6f5e0d58e203f72cccfd9ae1d2669e7bf50d
DanielYanger <danielyang2002@gmail.com>
2020-06-25 15:54:12 -0500

    MongoDB

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const express = require("express");
+const dbURI =
+  "mongodb+srv://daniel_yanger:test1234@netninjaproject-zfi60.mongodb.net/NetNinjaTutorial?retryWrites=true&w=majority";
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.listen(3000);
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.send("<p>about page</p>");
+  res.render("about", { title: "About" });
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
+// 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
