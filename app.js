2ab497ce8a5939e4131686e5d3d6443f23c91e17
DanielYanger <danielyang2002@gmail.com>
2020-06-25 19:38:39 -0500

    Yeet

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+// express app
+const app = express();
 const dbURI =
   "mongodb+srv://daniel_yanger:test1234@netninjaproject-zfi60.mongodb.net/NetNinjaTutorial?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log("Error"));
 
-const app = express();
+// listen for requests
+
+// register view engine
 app.set("view engine", "ejs");
 
-app.listen(3000);
-
+// middleware & static files
 app.use(express.static("public"));
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 app.get("/", (req, res) => {
   const blogs = [
